617ea3b3d14c899c04b3a6e70a1da7fda7f1de21
Piglet719 <bobo190790@gmail.com>
2026-04-08 21:29:57 +0800

    fix: add watch configuration for Docker Compose server setup

--- Taipei-City-Dashboard-FE/vite.config.js
+++ Taipei-City-Dashboard-FE/vite.config.js
@@ -10,6 +10,10 @@ const serverConfig = isDockerCompose
 		// Docker Compose override config
 		host: "0.0.0.0",
 		port: 80, // 如有需要可變更 port
+		watch: {
+			usePolling: true,
+			interval: 300
+		},
 		proxy: {
 			"/api/dev": {
 				target: "http://dashboard-be:8080",
